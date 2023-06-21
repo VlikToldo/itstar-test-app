be95944d27ae3c10f1c7367bee58717ebfc1938c
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-06-22 02:55:37 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/wallet">
+        <BrowserRouter basename="/itstar-test-app">
           <App />
         </BrowserRouter>
       </PersistGate>
