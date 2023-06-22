df174c870e5b7cc3addd31c3d8b79d4036204c08
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-06-22 03:04:26 +0300

    test2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,22 @@
-import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Layout from './Layout/Layout';
+import Layout from "./Layout/Layout";
 
-const TransactionHistoryPage = lazy(() => import('../pages/TransactionHistoryPage/TransactionHistoryPage'));
-const WalletPage = lazy(() => import('../pages/WalletPage/WalletPage'));
-
-
-
+const TransactionHistoryPage = lazy(() =>
+  import("../pages/TransactionHistoryPage/TransactionHistoryPage")
+);
+const WalletPage = lazy(() => import("../pages/WalletPage/WalletPage"));
 
 const App = () => {
-
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<WalletPage/>}/>
-        <Route path='transactions-history' element={<TransactionHistoryPage/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WalletPage />} />
+        <Route
+          path="transactions-history"
+          element={<TransactionHistoryPage />}
+        />
       </Route>
     </Routes>
   );
